36b9047524df4c3675094911a2020fd1d8c99175
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-01-26 14:07:51 +0000

    update

---
+++ 1-first.py/8-iloc.py
@@ -0,0 +1,11 @@
+import pandas as pd  
+
+data  = { 
+    "Name" : ["suresh" , "ram" ,"shaym", "hari","kiran","rahul"],
+    "Age":[30,40,19,40,39,20],
+    "Salary":[50000,40000,31000,49000,40000,5900],
+    "PerformanceScore": [85,90,78,92,88,95]
+}
+
+df  =pd.DataFrame(data)
+print(df.iloc[1:3])
