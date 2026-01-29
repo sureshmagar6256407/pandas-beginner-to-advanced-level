6dcbbad64597a7d38c025e38aaf5db074ef75ab9
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-01-29 16:44:42 +0545

    grouping by

---
+++ 4-sorting &aggrgation/3-groupin.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data  = { 
+    "Name": ["Alice","Rahul", "Bob", "Charlie","tekam", "David"],
+    "Age": [25, 30, 35, 40, 28, 32],
+    "City": ["New York", "Los Angeles", "Chicago", "Houston", "ghorahi","dang"],
+    "Salary": [50000, 60000, 70000, 80000, 55000, 65000]
+}
+df = pd.DataFrame(data)
+group   =df.groupby("City")["Salary"].sum()
+print(group)
---
+++ 4-sorting &aggrgation/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data  = { 
+    "Name": ["Alice","Rahul", "Bob", "Charlie","tekam", "David"],
+    "Age": [25, 30, 35, 40, 28, 32],
+    "City": ["New York", "Los Angeles", "Chicago", "Houston", "ghorahi","dang"],
+    "Salary": [50000, 60000, 70000, 80000, 55000, 65000]
+}
